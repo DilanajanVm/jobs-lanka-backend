41e8ecac7b7a25448f82fa1e324b3748feb2a797
Dilanjana Vimukthi <dilanjana@webmotech.com>
2023-08-02 12:16:02 +0530

    initialize projet

---
+++ src/main/java/com/backend/jobslanka/jobslanka/JobsLankaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.jobslanka.jobslanka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsLankaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsLankaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/jobslanka/jobslanka/JobsLankaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.jobslanka.jobslanka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsLankaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
